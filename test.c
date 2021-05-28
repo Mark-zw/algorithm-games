f4d64adadb476871d80511205a9c910dd18b3b21
Student Zhang <79700308+zhangwu1997@users.noreply.github.com>
2021-05-29 07:35:13 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,43 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS 1 
-#include "SeqList.h"
-
-void TestSeqList1()
-{
-	SeqList s = { 0 };
-	SeqListInit(&s);//打个断点在这，直接按F5跳到这里
-	SeqListPushBack(&s, 1);
-	SeqListPushBack(&s, 2);
-	SeqListPushBack(&s, 3);
-	SeqListPushBack(&s, 4);
-	SeqListPushBack(&s, 5);
-	SeqListPushBack(&s, 6);//越界了不一定会把错误报出来，酒驾模型,需要达到一定的临界值才能报出来
-	SeqListPushBack(&s, 7);
-	SeqListPushBack(&s, 8);
-	SeqListPushBack(&s, 9);
-	SeqListPrint(&s);
-	printf("\n");
-
-	SeqListPopBack(&s);
-	SeqListPopBack(&s);
-	SeqListPopBack(&s);
-	SeqListPrint(&s);
-	printf("\n");
-
-	SeqListPushFront(&s, -1);
-	SeqListPushFront(&s, -2);
-	SeqListPushFront(&s, -3);
-	SeqListPrint(&s);
-	printf("\n");
-
-	SeqListPopFront(&s);
-	SeqListPopFront(&s);
-	SeqListPrint(&s);
-	printf("\n");
-}
-
+#include "SList.h"
 int main()
 {
-	TestSeqList1();
+	SListNode* pList = NULL;//刷题的时候一般就是给一个头指针
+	SListPushBack(&pList, 1);
+	SListPushBack(&pList, 2);
+	SListPushBack(&pList, 3);
+	SListPushBack(&pList, 4);
+
+	SListPrint(pList);
 	return 0;
 }
-
